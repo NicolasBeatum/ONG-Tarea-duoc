2ce9a3e13f3f9379823391327120c2200a633228
Nicolás Andrés Hernández Vargas <85108017+NicolasBeatum@users.noreply.github.com>
2024-06-11 10:12:02 -0400

    Modelos hecho
    django cosas

--- Adopta/models.py
+++ Adopta/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class gato(models.Model):
+    cod_gato=models.IntegerField(primary_key=True)
+    nombre_gato=models.CharField(max_length=20)
+    edad_gato=models.IntegerField()
+    raza_gato=models.CharField(max_length=20)
+    esterelizacion_gato=models.BooleanField(True)
+    fecha_nac=models.DateField(blank=True,null=True)
+
+    def __str__(self):
+        return self.nombre_gato
